ab3b36aabfc64adbddc2f341b6465c6a58ca3496
Stephan Fowler <stephanfowler@gmail.com>
2013-12-28 16:06:11 +0000

    Proxy to Ophan

--- app.js
+++ app.js
@@ -7,10 +7,50 @@ var Canvas = require('canvas')
   , canvas = new Canvas(100, 100)
   , c = canvas.getContext('2d')
   , http = require('http')
-  , fs = require('fs');
+  , url = require('url')
+  , fs = require('fs')
+  , _ = require('lodash');
 
+// e.g. http://localhost:3000/?path=/world/2013/dec/27/judge-rules-nsa-phone-data-collection-legal
 http.createServer(function (req, res) {
+    var params = url.parse(req.url, true).query,
+        raw;
+    
+    if (!params.path) {
+        res.end();
+        return;
+    }
 
+    var options = {
+      host: 'api.ophan.co.uk',
+      path: '/api/breakdown?path=' + params.path
+    };
+
+    callback = function(response) {
+        var str = '';
+
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function () {
+            var rawData;
+
+            try {
+                rawData = JSON.parse(str);
+            } catch(e) {
+                rawData = {};
+            }
+            if (rawData.totalHits) {
+                console.log(rawData.totalHits + ' HITS');
+            };
+            res.end();
+        });
+    }
+
+    http.request(options, callback).end();
+
+    /*
     c.beginPath();
     c.moveTo(0, 0);
     c.lineTo(100, 0);
@@ -23,6 +63,7 @@ http.createServer(function (req, res) {
         res.writeHead(200, { 'Content-Type': 'image/png', 'Content-Length': buf.length });
         res.end(buf);
     });
+    */
 
 }).listen(3000);
 
