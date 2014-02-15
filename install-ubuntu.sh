39067aac955a71c941d82382df292889b09456f6
Stephan Fowler <stephanfowler@gmail.com>
2014-02-15 18:20:54 +0000

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -4,9 +4,11 @@ sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-po
 
 cd
 
+sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update 
 sudo apt-get -y install git-core
-sudo apt-get -y install nodejs npm
+sudo apt-get install python-software-properties python g++ make
+sudo apt-get install nodejs
 sudo apt-get -y install libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
 
 git clone https://github.com/stephanfowler/ophan-sparklines.git
