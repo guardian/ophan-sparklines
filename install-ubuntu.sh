9cbab69a0f141328306ae453f83b469f4143eed9
Stephan Fowler <stephanfowler@gmail.com>
2014-02-15 17:44:19 +0000

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -4,11 +4,9 @@ cd
 
 sudo apt-get update 
 
-sudo apt-get install git-core
-sudo apt-get install nodejs
-
-
-sudo apt-get install libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
+sudo apt-get -y install git-core
+sudo apt-get -y install nodejs
+sudo apt-get -y install libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
 
 git clone git@github.com:stephanfowler/ophan-sparklines.git
 
