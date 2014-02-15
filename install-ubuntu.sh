302735b26154510c3d3237bb62f69c4b6a970e9f
Stephan Fowler <stephanfowler@gmail.com>
2014-02-15 17:50:37 +0000

    Fixes to install script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -3,7 +3,6 @@
 cd
 
 sudo apt-get update 
-
 sudo apt-get -y install git-core
 sudo apt-get -y install nodejs npm
 sudo apt-get -y install libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
